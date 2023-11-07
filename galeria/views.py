d7cb05f49f39c6f00543c4c3e4ffa3deee366a4d
Matheus Pinter <pintermaciel@gmail.com>
2023-11-06 22:27:53 -0300

    aprendendo sobre o admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = fotografia.objects.filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards" : fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    foto = get_object_or_404(fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"foto": foto})
